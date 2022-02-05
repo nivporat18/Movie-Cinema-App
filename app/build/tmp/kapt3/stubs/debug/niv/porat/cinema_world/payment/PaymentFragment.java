7dcbae0459a690abb5de30c48c04574465907338
Niv Porat <nivporat18@icloud.com>
2022-02-05 22:15:43 +0200

    second

--- app/build/tmp/kapt3/stubs/debug/niv/porat/cinema_world/payment/PaymentFragment.java
+++ app/build/tmp/kapt3/stubs/debug/niv/porat/cinema_world/payment/PaymentFragment.java
@@ -2,7 +2,7 @@ package niv.porat.cinema_world.payment;
 
 import java.lang.System;
 
-@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000@\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\b\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0002\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J$\u0010\u0010\u001a\u00020\u00112\u0006\u0010\u0012\u001a\u00020\u00132\b\u0010\u0014\u001a\u0004\u0018\u00010\u00152\b\u0010\u0016\u001a\u0004\u0018\u00010\u0017H\u0016J\u001a\u0010\u0018\u001a\u00020\u00192\u0006\u0010\u001a\u001a\u00020\u00112\b\u0010\u0016\u001a\u0004\u0018\u00010\u0017H\u0016R\u0010\u0010\u0003\u001a\u0004\u0018\u00010\u0004X\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u001b\u0010\u0005\u001a\u00020\u00068BX\u0082\u0084\u0002\u00a2\u0006\f\n\u0004\b\t\u0010\n\u001a\u0004\b\u0007\u0010\bR\u0011\u0010\u000b\u001a\u00020\u00048F\u00a2\u0006\u0006\u001a\u0004\b\f\u0010\rR\u000e\u0010\u000e\u001a\u00020\u000fX\u0082.\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u001b"}, d2 = {"Lniv/porat/cinema_world/payment/PaymentFragment;", "Landroidx/fragment/app/Fragment;", "()V", "_binding", "Lniv/porat/cinema_world/databinding/PaymentFragmentBinding;", "args", "Lniv/porat/cinema_world/payment/PaymentFragmentArgs;", "getArgs", "()Lniv/porat/cinema_world/payment/PaymentFragmentArgs;", "args$delegate", "Landroidx/navigation/NavArgsLazy;", "binding", "getBinding", "()Lniv/porat/cinema_world/databinding/PaymentFragmentBinding;", "viewModel", "Lniv/porat/cinema_world/payment/PaymentViewModel;", "onCreateView", "Landroid/view/View;", "inflater", "Landroid/view/LayoutInflater;", "container", "Landroid/view/ViewGroup;", "savedInstanceState", "Landroid/os/Bundle;", "onViewCreated", "", "view", "app_debug"})
+@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000H\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\b\n\u0002\u0010\u000e\n\u0002\b\t\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0002\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J$\u0010\u001a\u001a\u00020\u001b2\u0006\u0010\u001c\u001a\u00020\u001d2\b\u0010\u001e\u001a\u0004\u0018\u00010\u001f2\b\u0010 \u001a\u0004\u0018\u00010!H\u0016J\u001a\u0010\"\u001a\u00020#2\u0006\u0010$\u001a\u00020\u001b2\b\u0010 \u001a\u0004\u0018\u00010!H\u0016R\u0010\u0010\u0003\u001a\u0004\u0018\u00010\u0004X\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u001b\u0010\u0005\u001a\u00020\u00068BX\u0082\u0084\u0002\u00a2\u0006\f\n\u0004\b\t\u0010\n\u001a\u0004\b\u0007\u0010\bR\u0011\u0010\u000b\u001a\u00020\u00048F\u00a2\u0006\u0006\u001a\u0004\b\f\u0010\rR\u0011\u0010\u000e\u001a\u00020\u000f8F\u00a2\u0006\u0006\u001a\u0004\b\u0010\u0010\u0011R\u0011\u0010\u0012\u001a\u00020\u000f8F\u00a2\u0006\u0006\u001a\u0004\b\u0013\u0010\u0011R\u0011\u0010\u0014\u001a\u00020\u000f8F\u00a2\u0006\u0006\u001a\u0004\b\u0015\u0010\u0011R\u0011\u0010\u0016\u001a\u00020\u000f8F\u00a2\u0006\u0006\u001a\u0004\b\u0017\u0010\u0011R\u000e\u0010\u0018\u001a\u00020\u0019X\u0082.\u00a2\u0006\u0002\n\u0000\u00a8\u0006%"}, d2 = {"Lniv/porat/cinema_world/payment/PaymentFragment;", "Landroidx/fragment/app/Fragment;", "()V", "_binding", "Lniv/porat/cinema_world/databinding/PaymentFragmentBinding;", "args", "Lniv/porat/cinema_world/payment/PaymentFragmentArgs;", "getArgs", "()Lniv/porat/cinema_world/payment/PaymentFragmentArgs;", "args$delegate", "Landroidx/navigation/NavArgsLazy;", "binding", "getBinding", "()Lniv/porat/cinema_world/databinding/PaymentFragmentBinding;", "cvv", "", "getCvv", "()Ljava/lang/String;", "expiry", "getExpiry", "fullName", "getFullName", "numberCard", "getNumberCard", "viewModel", "Lniv/porat/cinema_world/payment/PaymentViewModel;", "onCreateView", "Landroid/view/View;", "inflater", "Landroid/view/LayoutInflater;", "container", "Landroid/view/ViewGroup;", "savedInstanceState", "Landroid/os/Bundle;", "onViewCreated", "", "view", "app_debug"})
 public final class PaymentFragment extends androidx.fragment.app.Fragment {
     private niv.porat.cinema_world.databinding.PaymentFragmentBinding _binding;
     private niv.porat.cinema_world.payment.PaymentViewModel viewModel;
@@ -19,6 +19,26 @@ public final class PaymentFragment extends androidx.fragment.app.Fragment {
     }
     
     private final niv.porat.cinema_world.payment.PaymentFragmentArgs getArgs() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getFullName() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getNumberCard() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getCvv() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getExpiry() {
         return null;
     }
     
--- app/src/main/java/niv/porat/cinema_world/payment/PaymentFragment.kt
+++ app/src/main/java/niv/porat/cinema_world/payment/PaymentFragment.kt
@@ -32,6 +32,11 @@ class PaymentFragment : Fragment() {
 
     private val args: PaymentFragmentArgs by navArgs()
 
+    val fullName get() = binding.etPaymentName.text.toString()
+    val numberCard get() = binding.etCardNumber.text.toString()
+    val cvv get() = binding.etCvv.text.toString()
+    val expiry get() = binding.etExpiry.text.toString()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -50,11 +55,7 @@ class PaymentFragment : Fragment() {
         val gson = Gson()
         val ticket = gson.fromJson(json, Ticket::class.java)
 
-        binding.TotalPay.text = "Price: ${ticket.numberOfTickets * 35}"
-        val fullName = binding.etPaymentName.text
-        val numberCard = binding.etCardNumber.text
-        val cvv = binding.etCvv.text
-        val expiry = binding.etExpiry.text
+        binding.TotalPay.text = "Price: ${ticket.numberOfTickets * 35}₪"
         binding.tvNumberTicket.text = "NumberTicket: ${ticket.numberOfTickets} "
         binding.locationMovie.text = "Location: ${ticket.location}"
         binding.nameMovie.text = "Movie: ${ticket.movie}"
@@ -81,31 +82,38 @@ class PaymentFragment : Fragment() {
         })
         binding.btnPay.setOnClickListener {
             hideKeyboard(this.requireActivity())
-            AlertDialog.Builder(this.requireContext()).setTitle("Confirm")
-                .setMessage("${ticket.numberOfTickets} Tickets for: ${ticket.movie}\n" +
-                        "Price: ${ticket.numberOfTickets * 35}\n" +
-                        "Location: ${ticket.location} \n" +
-                        "Time: ${ticket.time}\n" +
-                        "Full Name:  $fullName \n" +
-                        "Number Card: $numberCard \n" +
-                        "CVV:  $cvv \n" +
-                        "Expiry: $expiry\n")
-                .setPositiveButton("Confirm") { _, _ ->
-                    Toast.makeText(requireContext(),
-                        "Thanks For Purchase, Enjoy The Movie ",
-                        Toast.LENGTH_LONG).show()
-                    PaymentViewModel.addToPayment(Payment(name = fullName.toString(),
-                        totalPrice = (ticket.numberOfTickets * 35),
-                        cardNumber = numberCard.toString(),
-                        cvv = cvv.toString(),
-                        expiry = expiry.toString()))
-                    findNavController().navigate(R.id.action_paymentFragment_to_navigation_home)
-                }
-                .setNegativeButton("Cancel") { _, _ ->
+            if (fullName == "" || numberCard == "" || expiry == "" || cvv == ""  ) {
+                AlertDialog.Builder(this.requireContext()).setTitle("Error")
+                    .setMessage("Please fill in all the details")
+                    .setPositiveButton("OK") { _, _ -> }
+                    .show()
 
-                }.show()
+            } else {
+                AlertDialog.Builder(this.requireContext()).setTitle("Confirm")
+                    .setMessage("${ticket.numberOfTickets} Tickets for: ${ticket.movie}\n" +
+                            "Price: ${ticket.numberOfTickets * 35}₪\n" +
+                            "Location: ${ticket.location} \n" +
+                            "Time: ${ticket.time}\n" +
+                            "Full Name:  $fullName \n" +
+                            "Number Card: $numberCard \n" +
+                            "CVV:  $cvv \n" +
+                            "Expiry: $expiry\n")
+                    .setPositiveButton("Confirm") { _, _ ->
+                        Toast.makeText(requireContext(),
+                            "Thanks For Purchase, Enjoy The Movie ",
+                            Toast.LENGTH_LONG).show()
+                        PaymentViewModel.addToPayment(Payment(name = fullName.toString(),
+                            totalPrice = (ticket.numberOfTickets * 35),
+                            cardNumber = numberCard.toString(),
+                            cvv = cvv.toString(),
+                            expiry = expiry.toString()))
+                        findNavController().navigate(R.id.action_paymentFragment_to_navigation_home)
+                    }
+                    .setNegativeButton("Cancel") { _, _ ->
 
+                    }.show()
 
+            }
         }
 
         binding.btnChancel.setOnClickListener {
